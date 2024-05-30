5cbef484271447337cd81fc4e4ab4faf1e6b0b9b
Guillermo Call <cobbgcall@gmail.com>
2024-05-30 16:06:22 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "google" {
+    project                         = "mimetic-retina-410109"
+    region                          = "us-central1"     
+}
+
+resource "google_compute_network" "peering_network" {
+    name                            = "peering-network"
+    auto_create_subnetworks         = false 
+}
+
+resource "google_compute_subnetwork" "peering_subnet" {
+    name                            = "peering-subnet"
+    ip_cidr_range                   = "10.10.0.0/24"
+    network                         = google_compute_network.peering_network.id
+    private_ip_google_access        = true
+    region                          = "us-central1" 
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+    name                            = "private-ip-address"
+    purpose                         = "VPC_PEERING"
+    address_type                    = "INTERNAL"
+    prefix_length                   = 24
+    network                         = google_compute_network.peering_network.id
+    address                         = "10.101.0.100"
+}
+
+resource "google_service_networking_connection" "peering_connection" {
+    network                         = google_compute_network.peering_network.id
+    service                         = "servicenetworking.googleapis.com"
+    reserved_peering_ranges         = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "db_instance" {
+    name                            = "db-instance"
+    region                          = "us-central1"
+    database_version                = "POSTGRES_14"
+
+    depends_on                      = [google_service_networking_connection.peering_connection]
+
+    settings {
+        tier                        = "db-f1-micro"
+        ip_configuration {
+          ipv4_enabled              = false
+          private_network           = google_compute_network.peering_network.id
+        }   
+    }
+
+    deletion_protection             = false 
+}
