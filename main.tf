f46275164ad3651b719cbe699908d740a23d72c7
Guillermo Call <cobbgcall@gmail.com>
2024-06-07 17:05:59 +0200

    New approach

--- main.tf
+++ main.tf
@@ -20,7 +20,8 @@ resource "google_compute_address" "private_ip_address" {
     name                            = "private-ip-address"
     purpose                         = "VPC_PEERING"
     address_type                    = "INTERNAL"
-    prefix_length                   = 16 
+    prefix_length                   = 16
+    network                         = google_compute_network.peering_network.id 
 }
 
 resource "google_service_networking_connection" "snc_private_ip" {
