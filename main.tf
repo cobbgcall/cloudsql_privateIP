d69109324bb0af449202a353c794eeac7ff9648a
Guillermo Call <cobbgcall@gmail.com>
2024-06-11 11:52:15 +0200

    New approach

--- main.tf
+++ main.tf
@@ -3,33 +3,10 @@ provider "google" {
     region                          = "us-central1"     
 }
 
-resource "google_compute_network" "peering_network" {
-    name                            = "peering-network"
-    auto_create_subnetworks         = false 
-}
-
-resource "google_compute_subnetwork" "peering_subnet" {
-    name                            = "peering-subnet"
-    ip_cidr_range                   = "10.10.0.0/24"
-    network                         = google_compute_network.peering_network.id
-    private_ip_google_access        = true
-    region                          = "us-central1" 
-}
-
-resource "google_compute_address" "private_ip_address" {
-    name                            = "private-ip-address"
-    purpose                         = "GCE_ENDPOINT"
-    address_type                    = "INTERNAL"
-    address                         = "10.10.0.24"
-    network                         = google_compute_network.peering_network.id
-}
-
-resource "google_service_networking_connection" "snc_private_ip" {
-    network                         = google_compute_network.peering_network.id
-    service                         = "servicenetworking.googleapis.com"
-    reserved_peering_ranges         = [google_compute_address.private_ip_address.name]
-
-    depends_on = [ google_compute_address.private_ip_address ]
+locals {
+    service_list = [ 
+        "compute.googleapis.com"
+    ]
 }
 
 resource "google_sql_database_instance" "db_instance" {
@@ -42,20 +19,87 @@ resource "google_sql_database_instance" "db_instance" {
         availability_type           = "REGIONAL"
         ip_configuration {
             ipv4_enabled            = false
-            private_network         = google_compute_network.peering_network.id    
+            psc_config {
+              psc_enabled           = true
+              allowed_consumer_projects = [ "mimetic-retina-410109" ] 
+            }       
         }   
     }
 
-    depends_on = [ google_service_networking_connection.snc_private_ip ]
-
     deletion_protection             = false 
+}
+
+resource "google_sql_user" "db_user" {
+    project                         = ""
+    name                            = "db-user"
+    instance                        = google_sql_database_instance.db_instance.name
+    host                            = "%"
+    password                        = random_password.postgres_password.result 
+}
+
+resource "random_password" "postgres_password" {
+    length                          = 8 
+}
+
+resource "google_compute_network" "peering_network" {
+    name                            = "peering-network"
+    auto_create_subnetworks         = false 
+}
+
+resource "google_compute_subnetwork" "peering_subnet" {
+    name                            = "peering-subnet"
+    ip_cidr_range                   = "10.10.0.0/24"
+    network                         = google_compute_network.peering_network.id
+    private_ip_google_access        = true
+    region                          = "us-central1"
+}
+
+resource "google_compute_router" "peering_router" {
+    name                            = "peering-router"
+    network                         = google_compute_network.peering_network.id
+    region                          = "us-central1"  
+}
+
+resource "google_compute_router_nat" "peering_nat" {
+    project                         = "mimetic-retina-410109" 
+    name                            = "peering-nat"
+    router                          = google_compute_router.peering_router.name
+    region                          = "us-central1"
+    nat_ip_allocate_option          = "AUTO_ONLY" 
+    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+resource "google_compute_firewall" "peering_fw" {
+    project                         = "mimetic-retina-410109"
+    name                            = "peering-fw"
+    network                         = google_compute_network.peering_network.id
+    direction                       = "INGRESS"
+    priority                        = "1000"
+    allow {
+        protocol = "TCP"
+        ports = ["22"] 
+        }
+    source_ranges                   = ["47.63.171.210/32"]
+    target_tags                     = ["peering-fw"]
+}
+
+resource "google_compute_address" "private_ip_address" {
+    name                            = "private-ip-address"
+    subnetwork                      = google_compute_subnetwork.peering_subnet.id 
+    address_type                    = "INTERNAL"
+    address                         = "10.10.0.24"
+    region                          = "us-central1"
+
+    depends_on = [ google_sql_database_instance.db_instance ]
+
 }
 
 resource "google_compute_forwarding_rule" "psc_forwarding_rule" {
     name                            = "psc-forwarding-rule"
     region                          = "us-central1"
+    load_balancing_scheme           = "" 
     network                         = google_compute_network.peering_network.id
-    ip_address                      = google_compute_address.private_ip_address.self_link
+    ip_address                      = google_compute_address.private_ip_address.id
     target                          = google_sql_database_instance.db_instance.psc_service_attachment_link
 
     depends_on = [ google_compute_address.private_ip_address ]
